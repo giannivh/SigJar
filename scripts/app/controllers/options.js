82a101b2bc9a68d62e9cde1ba960aeeb50b1886b
Gianni Van Hoecke <gianni.vanhoecke@lin-k.net>
2016-01-23 15:52:23 +0100

    Add and remove signatures

--- scripts/app/controllers/options.js
+++ scripts/app/controllers/options.js
@@ -39,6 +39,49 @@ angular.module('sigjar')
           $scope.options.selectedSignature = signature;
       };
 
+      $scope.createSignature = function() {
+
+          $scope.options.signatures.push(
+              {
+                  name: 'Signature ' + ($scope.options.signatures.length + 1),
+                  code: '<p>Kind regards,<br/>John Doe</p>'
+              }
+          );
+
+          $scope.options.selectedSignature = $scope.options.signatures[$scope.options.signatures.length-1];
+
+          $scope.saveOptions( 'The signature has been created.' );
+      };
+
+      $scope.removeSignature = function() {
+
+          if ($scope.options.signatures.length == 1) {
+
+              return;
+          }
+
+          var index = -1;
+          for (var i = 0; i < $scope.options.signatures.length; i++) {
+
+              var sig = $scope.options.signatures[i];
+
+              if (sig.name == $scope.options.selectedSignature.name) {
+
+                  index = i;
+                  break;
+              }
+          }
+
+          if (i > -1) {
+
+              $scope.options.signatures.splice( index, 1 );
+          }
+
+          $scope.options.selectedSignature = $scope.options.signatures[index-1];
+
+          $scope.saveOptions( 'The signature has been removed.' );
+      };
+
       $scope.getSignaturePreview = function() {
 
           return $sce.trustAsHtml( $scope.options.selectedSignature.code );
@@ -64,7 +107,9 @@ angular.module('sigjar')
           );
       };
 
-      $scope.saveOptions = function() {
+      $scope.saveOptions = function( message ) {
+
+          message = message || 'Options saved.';
 
           chrome.storage.sync.set(
               { signatures: $scope.options.signatures },
@@ -73,7 +118,7 @@ angular.module('sigjar')
                   $scope.$apply(function () {
 
                       //Update status to let user know options were saved.
-                      $scope.options.feedback = { code: 'OK', message: 'Options saved.' };
+                      $scope.options.feedback = { code: 'OK', message: message };
                       $timeout( function() { $scope.resetFeedback(); }, 3000 );
                   });
               }
