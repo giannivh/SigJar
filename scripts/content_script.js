b37c04f896cd163fff811615aed6cb1890e30201
Gianni Van Hoecke <gianni.vanhoecke@lin-k.net>
2016-01-26 21:33:25 +0100

    mail.live.com (oulook.com) support: it runs inside an iFrame

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -2,8 +2,18 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 
     if (request.action == "insertSig") {
 
-		//Get selection or current caret position
-		var range = window.getSelection().getRangeAt( 0 );
+        //Get selection or current caret position
+        var range = window.getSelection().getRangeAt( 0 );
+
+        if (window.location.hostname.indexOf( 'mail.live' ) > -1) {
+
+            //If outlook.com (mail.live.com), then get the iframe first.
+            var frame = document.getElementById( 'ComposeRteEditor_surface' );
+            var frameWindow = frame && frame.contentWindow;
+            var frameDocument = frameWindow && frameWindow.document;
+            var selection = frameDocument.getSelection() || frameWindow.getSelection();
+            range = selection.getRangeAt( 0 );
+        }
 
 		//Create the signature element
 		var sigElement = document.createElement( "div" );
